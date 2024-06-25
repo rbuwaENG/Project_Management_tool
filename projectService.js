3ab9cb15064bdad027998faa4e152075a8b09770
rbuwaENG <en97548@foe.sjp.lk>
2024-06-25 17:42:06 +0530

    update

--- projectService.js
+++ projectService.js
@@ -2,69 +2,6 @@
 import Project from './project.js';
 
 class ProjectService {
-  constructor() {
-    this.projectslist = [];
-    this.readJson();
-    this.getAll();
-  }
-
-<<<<<<< HEAD
-// read json 
-  async readJson(){
-    const response = await fetch('project.json')
-    const data  =  await response.json()
-    this.projectslist = data.projects.map(detail => {
-      console.log(detail.id);
-      return new Project(detail.id, detail.name, detail.revenue, detail.status);
-      
-    });
-    console.log(this.projectslist.length)
-  }
-    //update in cosole 
-    //data.projects.forEach(projectsInFile => {
-      //console.log(projectsInFile.id);
-      //console.log(projectsInFile.name);});
-   
-    
-
-//function 
- getAll() {
-  
-  //console.log(returnList);
-  /*this.readjson().forEach(detail => {
-    //new Project(detail.id,detail.name, 20000,'On going')
-    console.log(detail.name);
-  });*/
-      //hard coded array of projectcs 
-      // id ||  name || revenue || status
-  
-  // const projects = [
-    
-   
- //  ];
-
-   
-    //return projects;
-   // return this.readJson().then(projectData => {
-   //   const projectlist = projectData.map(detail => {
-   //     return new Project(detail.id, detail.name, detail.revenue, detail.status);
-   //   });
-   //  const projects = projectlist;
-   //   console.log(projects);
-   //   return projects;
-  //  });
-  console.log(this.projectslist.length);
-  return this.projectslist;
-  if (this.projects.length === 0) {
-    // Return an initial hardcoded array if the projects are not yet loaded
-    return [
-      new Project(1, 'Loading...', 0, 'Loading...')
-    ];
-  }
- 
-}
-    
-=======
   async readjson() {
     const response = await fetch('project.json');
     const data = await response.json();
@@ -72,19 +9,12 @@ class ProjectService {
     data.projects.forEach(projectData => {
       projects.push(new Project(projectData.id, projectData.name, projectData.revenue, projectData.status));
     });
-    return projects;
->>>>>>> feature/FI-690-read-Jason-file
-  }
-
-<<<<<<< HEAD
-
-
-export default ProjectService;
-=======
-async getAll() {
-  const projects = await this.readjson();
-  return projects;
-  }
-}
-export default ProjectService;
->>>>>>> feature/FI-690-read-Jason-file
+    
+    return projects;}
+    async getAll() {
+      const projects = await this.readjson();
+      return projects;
+      }
+    }
+    
+    export default ProjectService;
