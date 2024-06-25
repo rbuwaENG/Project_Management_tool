22a9afe0f0752d32df7eb2a14fa2f6d65bd6cc5a
rbuwaENG <en97548@foe.sjp.lk>
2024-06-25 12:32:58 +0530

    update $ sign

--- projectService.js
+++ projectService.js
@@ -8,13 +8,15 @@ class ProjectService {
       // id ||  name || revenue || status
     const projects = [
       new Project(1, 'Project 1', 100000,'Completed'),
-      new Project(2, 'Project 2', 150000,'Completed'),
-      new Project(3, 'Project 2.01', 200000, 'Ongoing'),
+      new Project(2, 'Command Program', 150000,'Completed'),
+      new Project(3, 'Project Point', 200000, 'Ongoing'),
       new Project(4, 'Project 2.1', 200000, 'Ongoing'),
       new Project(5, 'Project 3', 200000, 'Ongoing'),
     ];
     return projects;
   }
 }
+
+
 
 export default ProjectService;
