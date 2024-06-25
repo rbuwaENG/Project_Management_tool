75a8cb78d113713e0ec2847641ccc09ecffccc9a
Asal Samarasinghe <asalsamarasinghe30@gmail.com>
2024-06-25 12:33:32 +0530

    adding dummy  data

--- projectService.js
+++ projectService.js
@@ -7,11 +7,13 @@ class ProjectService {
       //hard coded array of projectcs 
       // id ||  name || revenue || status
     const projects = [
-      new Project(1, 'Project 1', 100000,'Completed'),
-      new Project(2, 'Project 2', 150000,'Completed'),
-      new Project(3, 'Project 2.01', 200000, 'Ongoing'),
-      new Project(4, 'Project 2.1', 200000, 'Ongoing'),
-      new Project(5, 'Project 3', 200000, 'Ongoing'),
+      new Project(1, 'Project Breeze', 20000,'On going'),
+      new Project(2, 'Command Program', 10000,'On going'),
+      new Project(3, 'Project Point', 15000, 'Completed'),
+      new Project(4, 'Project Mecha', 8000, 'On going'),
+      new Project(5, 'Program Pad', 16000, 'On going'),
+      new Project(6, 'Project Synergy', 22000, 'Completed'),
+      new Project(7, 'Dynamic Program', 31000, 'On going'),
     ];
     return projects;
   }
